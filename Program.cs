47f29ff97a7fe4afa76544040bd4ebd41683b404
gyozo-sz <gyozo.szabo@nixs.com>
2024-05-06 16:50:35 +0200

    4.2.1 practical task

--- Program.cs
+++ Program.cs
@@ -2,18 +2,39 @@
 
 namespace MyApp
 {
+    class User
+    {
+        private readonly string _userId;
+        private readonly string _name;
+        private uint _age;
+
+        public User (string userId, string name, uint age)
+        {
+            _userId = userId;
+            _name = name;
+            _age = age;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("User Info:");
+            Console.WriteLine($"\tID: {_userId}");
+            Console.WriteLine($"\tName: {_name}");
+            Console.WriteLine($"\tAge: {_age}");
+        }
+
+        public static void ChangeUserAge(ref User u, uint newAge)
+        {
+            u._age = newAge;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Start of the World!");
-            int i = 0;
-            while (i < 5)
-            {
-                Console.WriteLine("Hello World!");
-                ++i;
-            }
-            Console.WriteLine("End of the World!");
+            User u = new User("ID#001", "John Snow", 42);
+            User.ChangeUserAge(ref u, 24);
+            u.PrintInfo();
         }
     }
 }
