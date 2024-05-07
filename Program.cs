59eb0d0888755e527df07947070ebe4c6ee50b52
gyozo-sz <gyozo.szabo@nixs.com>
2024-05-07 11:27:17 +0200

    Encapsulated coffee class

--- Program.cs
+++ Program.cs
@@ -1,19 +1,60 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace MyApp
 {
+    class Coffee
+    {
+        private  string _name;
+        private  int _price;
+        public string Name { 
+            get => _name;
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    _name = value;
+                }
+            }
+        }
+        public int Price
+        {
+            get => _price;
+            set
+            {
+                if (value > 0)
+                {
+                    _price = value;
+                }
+            }
+        }
+
+        public Coffee(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        private void GetInfo()
+        {
+            Console.WriteLine("Coffee info:");
+            Console.WriteLine($"\tName: {Name}");
+            Console.WriteLine($"\tPrice: {Price}");
+        }
+
+        public void PrintInfo()
+        {
+            GetInfo();
+            Console.WriteLine("-----------------------------");
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Start of the World!");
-            int i = 0;
-            while (i < 5)
-            {
-                Console.WriteLine("Hello World!");
-                ++i;
-            }
-            Console.WriteLine("End of the World!");
+            Coffee cappuccino = new("Cappuccino", 50);
+            cappuccino.PrintInfo();
         }
     }
 }
