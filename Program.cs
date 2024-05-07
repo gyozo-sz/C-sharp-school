a2d507a1897de215d5b061f665df9ae1eeda4c16
gyozo-sz <gyozo.szabo@nixs.com>
2024-05-07 10:39:37 +0200

    Messenger class with constructors added

--- Program.cs
+++ Program.cs
@@ -2,18 +2,65 @@
 
 namespace MyApp
 {
+    enum MessengerState
+    {
+        StateOne,
+        StateTwo, 
+        StateThree, 
+        StateFour
+    }
+
+    class Messenger
+    {
+        static MessengerState _state;
+        string _message;
+        int _messageCounter;
+
+        public MessengerState State { get { return _state; } }
+
+        static Messenger()
+        {
+            _state = MessengerState.StateOne;
+        }
+
+        public Messenger(string message, int messageCounter)
+        {
+            this._message = message;
+            this._messageCounter = messageCounter;
+        }
+
+        public void ChangeState(MessengerState newState)
+        {
+            _state = newState;
+        }
+
+        internal void Deconstruct(out string message, out int messageCounter) { 
+            message = _message;
+            messageCounter = _messageCounter;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Start of the World!");
-            int i = 0;
-            while (i < 5)
-            {
-                Console.WriteLine("Hello World!");
-                ++i;
-            }
-            Console.WriteLine("End of the World!");
+            Messenger msg = new Messenger("some message", 30);
+            Messenger msg2 = new Messenger("some other message", 0);
+            (string message, int  msgCounter) = msg;
+
+            Console.WriteLine(message);
+            Console.WriteLine(msgCounter);
+
+            Console.WriteLine("\nMessenger states:");
+            Console.WriteLine($"Messenger 1: {msg.State}");
+            Console.WriteLine($"Messenger 2: {msg2.State}");
+
+            Console.WriteLine("\nChanging messenger states after changing the state using Messenger 1:");
+            msg.ChangeState(MessengerState.StateThree);
+            Console.WriteLine($"Messenger 1: {msg.State}");
+            Console.WriteLine($"Messenger 2: {msg2.State}");
+
+
         }
     }
 }
