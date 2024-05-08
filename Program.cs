c720282d99a63db9738c17b388304cfe81ae2922
gyozo-sz <gyozo.szabo@nixs.com>
2024-05-08 15:40:41 +0200

    User and generic Provider added

--- Program.cs
+++ Program.cs
@@ -2,18 +2,33 @@
 
 namespace MyApp
 {
+    class User
+    {
+        private int _userId;
+        private int _balance;
+
+        public int UserId { get => _userId; set => _userId = value; }
+        public int Balance { get => _balance; set => _balance = value; }
+    }
+
+    class Provider<U> where U: User
+    {
+        public void CheckBalance(U user)
+        {
+            Console.WriteLine($"Balance of the user is: {user.Balance}");
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Start of the World!");
-            int i = 0;
-            while (i < 5)
-            {
-                Console.WriteLine("Hello World!");
-                ++i;
-            }
-            Console.WriteLine("End of the World!");
+            User u = new();
+            u.UserId = 1234;
+            u.Balance = 1000;
+
+            Provider<User> p = new();
+            p.CheckBalance(u);
         }
     }
 }
